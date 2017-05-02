25c2c201baa160fcf5e8cf0aeb5cf09653ea52a8
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-05-02 16:40:01 -0700

    Adds createdAt field to user schema

--- models/user.js
+++ models/user.js
@@ -5,8 +5,9 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
+    createdAt: { type: Date, default: Date.now },
     username: String,
-    password: String
+    password: String,
 });
 
 //add passportLocalMongoose plugin to handle salting and hasing of user passwords
